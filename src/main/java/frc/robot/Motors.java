c70749d9ae2e7aa6c902057b77704583a22232a9
Elias Post Computer #3 <epost@bitbuckets.org>
2019-11-03 16:33:36 -0700

    Made the motor power go up and down.
    Repeatedly

---
+++ src/main/java/frc/robot/Motors.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class Motors {
+    
+    public static float motorPower = 0.0f;
+    public static WPI_TalonSRX motor1;
+    public static WPI_TalonSRX motor2;
+    public static WPI_TalonSRX motor3;
+    public static WPI_TalonSRX motor4;
+}
